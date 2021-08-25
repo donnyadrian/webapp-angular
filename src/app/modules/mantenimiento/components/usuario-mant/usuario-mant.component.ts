011aa7a9acdaa4dc2747241dd6f44a0340931ab3
donnyadrian <donnyadrian@zoho.com>
2021-08-25 08:49:35 -0500

    Dialog User is working with out services

--- src/app/modules/mantenimiento/components/usuario-mant/usuario-mant.component.ts
+++ src/app/modules/mantenimiento/components/usuario-mant/usuario-mant.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario.model';
 
@@ -9,103 +9,49 @@ import { Usuario } from 'src/app/models/usuario.model';
 })
 export class UsuarioMantComponent implements OnInit, OnDestroy {
 
-  @Input() usuarioDialog: boolean = false;
   submitted: boolean = false;
-  @Input() usuario: Usuario;
-  
+  @Input() usuario: Usuario | undefined;
+  @Output() closeDialogEvent: EventEmitter<Usuario> = new EventEmitter<Usuario>();
   public formUser: FormGroup;
   
   constructor(private fb: FormBuilder) {
-    this.usuario = {
-      id: 0,
-      idPersona: 0,
-      nombre: "",
-      login: "",
-      email: "",
-      esAd: 0,
-      fechaUltimaSesion: "",
-      estado: {
-        id:0,
-        descripcion:""
-      },
-      perfil: {
-        id: 0,
-        nombre: ""
-      }
-    };
     this.formUser = this.fb.group({
-      nombres: [null, [Validators.required]],
-      login: [null, Validators.required],
-      email: [null, [Validators.email, Validators.required]],
-      password: [null],
-      dominio: [null],
-      cambio: [null],
-      estado: [null],
-    });
-   }
-
-  ngOnInit(): void {
-    this.formUser.setValue({
-      nombres: [this.usuario.nombre],
-      login: [this.usuario.login],
-      email: [this.usuario.email],
-      password: [null],
-      dominio: [null],
-      cambio: [false],
-      estado: [this.usuario.estado.descripcion == 'ACTIVO'],
-    });
-    console.log("ngOnInit");
-  }
-
-  ngOnDestroy(){
-    console.log("fin");
-  }
-
-  initForm() {
-    this.formUser = this.fb.group({
-      nombres: [null, [Validators.required]],
-      login: [null, Validators.required],
-      email: [null, [Validators.email, Validators.required]],
-      password: [null],
+      nombres: ['', [Validators.required, Validators.minLength(3)]],
+      login: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
       dominio: [false],
       cambio: [false],
       estado: [false],
     });
   }
 
-  openNew() {
-    console.log("nuevo");
-    this.usuario = {
-      id: 0,
-      idPersona: 0,
-      nombre: "",
-      login: "",
-      email: "",
-      esAd: 0,
-      fechaUltimaSesion: "",
-      estado: {
-        id:0,
-        descripcion:""
-      },
-      perfil: {
-        id: 0,
-        nombre: ""
-      }
-    };
-    this.submitted = false;
-    this.usuarioDialog = true;
-    this.initForm();
-    
+  ngOnInit(): void {
+    if(this.usuario != undefined){
+      console.log("ngOnInit 2")
+      this.formUser.setValue({
+        nombres: this.usuario.nombre,
+        login: this.usuario.login,
+        email: this.usuario.email,
+        password: null,
+        dominio: false,
+        cambio: false,
+        estado: this.usuario.estado.descripcion == 'ACTIVO',
+      });
+    }
   }
 
+  ngOnDestroy(){  }
+
   hideDialog() {
-    this.usuarioDialog = false;
     this.submitted = false;
+    this.closeDialogEvent.emit(undefined);
   }
 
   saveProduct() {
     this.submitted = true;
+    if (this.formUser.status == "VALID") {
+      this.closeDialogEvent.emit(this.usuario);
+    }
   }
-
-
 }
--- src/app/modules/mantenimiento/components/usuario/usuario.component.ts
+++ src/app/modules/mantenimiento/components/usuario/usuario.component.ts
@@ -12,119 +12,40 @@ import { Usuario } from '../../../../models/usuario.model';
 })
 export class UsuarioComponent implements OnInit {
 
-  public formUser: FormGroup;
   usuarioDialog: boolean = false;
   usuarios: Usuario[] = [];
-  usuario: Usuario;
-  submitted: boolean = false;
+  usuario: Usuario |  undefined;
 
-  constructor(private usuarioService: UsuarioService, private messageService: MessageService, private confirmationService: ConfirmationService,
-    private fb: FormBuilder) { 
-    this.usuario = {
-      id: 0,
-      idPersona: 0,
-      nombre: "",
-      login: "",
-      email: "",
-      esAd: 0,
-      fechaUltimaSesion: "",
-      estado: {
-        id:0,
-        descripcion:""
-      },
-      perfil: {
-        id: 0,
-        nombre: ""
-      }
-    };
-    this.formUser = this.fb.group({
-      nombres: [null, [Validators.required]],
-      login: [null, Validators.required],
-      email: [null, [Validators.email, Validators.required]],
-      password: [null],
-      dominio: [null],
-      cambio: [null],
-      estado: [null],
-    });
-  }
+  constructor(private usuarioService: UsuarioService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
     this.getUsers();
-  }
-
-  initForm() {
-    this.formUser = this.fb.group({
-      nombres: [null, [Validators.required]],
-      login: [null, Validators.required],
-      email: [null, [Validators.email, Validators.required]],
-      password: [null],
-      dominio: [false],
-      cambio: [false],
-      estado: [false],
-    });
-
-    /*this.formUser.controls.state.valueChanges.subscribe((val) => {
-      console.log(val);
-    });
-
-    this.formUser.controls.address.valueChanges.subscribe((val) => {
-      console.log(val);
-    });*/
   }
 
   getUsers() {
     this.usuarioService.getAllUsers().subscribe(resp => {
       console.log(resp);
       this.usuarios = resp.usuarios;
-       //console.log(this.listaUsuarios)
     });
   }
 
   openNew() {
-    this.usuario = {
-      id: 0,
-      idPersona: 0,
-      nombre: "",
-      login: "",
-      email: "",
-      esAd: 0,
-      fechaUltimaSesion: "",
-      estado: {
-        id:0,
-        descripcion:""
-      },
-      perfil: {
-        id: 0,
-        nombre: ""
-      }
-    };
-    this.submitted = false;
+    this.usuario = undefined;
     this.usuarioDialog = true;
-    this.initForm();
+    console.log(this.usuario)
   }
 
   editUsuario(usuario: Usuario) {
     this.usuario = {...usuario};
-    console.log(this.usuario)
     this.usuarioDialog = true;
-    this.formUser.setValue({
-      nombres: [this.usuario.nombre],
-      login: [this.usuario.login],
-      email: [this.usuario.email],
-      password: [null],
-      dominio: [null],
-      cambio: [false],
-      estado: [this.usuario.estado.descripcion == 'ACTIVO'],
-    });
-    //this.formUser.controls. .setValue(this.usuario);
+    console.log(this.usuario)
   }
 
-  hideDialog() {
+  closeDialog(usuario : Usuario) {
     this.usuarioDialog = false;
-    this.submitted = false;
-  }
+    console.log(usuario)
+    if(usuario != undefined){
 
-  saveProduct() {
-    this.submitted = true;
+    }
   }
 }
